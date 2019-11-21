5b793f233f56f7049e9aa3500dcb56e6c094f2de
Mr.Kio <38723412+MrKioZ@users.noreply.github.com>
2019-11-21 05:39:59 +0200

    Create PyEngine.py

---
+++ PyEngine.py
@@ -0,0 +1,26 @@
+from requests import get
+from bs4 import BeautifulSoup
+import random
+
+class Crawler:
+
+    def Crawl(self, url):
+
+        with get(url) as response:
+            content = BeautifulSoup(response.text, "lxml")
+
+            for link in content.find_all("a"):
+                try:
+                    if link.attrs["href"].startswith('/'):
+                        yield url+link.attrs["href"]+'\n'
+                    elif link.attrs["href"].startswith('javascript:void(0)'):
+                        pass
+                    else:
+                        yield link.attrs["href"]+'\n'
+                except KeyError:
+                    pass
+
+if __name__ == '__main__':
+    bot = Crawler()
+    url = bot.Crawl(url='http://google.com')
+    print(next(url))
